f16cb2458289276ba167fc2d556968f0b8bd0228
B64283 <mattdarke@fullsail.edu>
2013-05-28 11:08:47 -0700

    String phone number pattern

---
+++ SDI_project_4.js
@@ -0,0 +1,28 @@
+alert("JavaScript works!");
+
+    
+  // arrays
+//Find the total value of just the numbers in an array, even if some of the items are not
+//numbers.
+
+    
+    
+    
+    
+    
+    
+//Does a string follow a 123-456-7890 pattern like a phone number?    
+  var myNumber = function("123 456 7890") {
+    var phonenumber = "123-456-7890";
+    var pattern = "012-456-8900";
+    if (pattern == "123-456-7890"){
+        console.log(true)
+    } else if { (pattern != "123-456-7890")
+        return console.log(false)
+    }    
+    
+    };  
+    
+    myNumber("123 456 7890")
+    
+    
