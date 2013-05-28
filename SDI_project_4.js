ef1c08d30f208ec51531b2096e2a5fabf729a33f
B64283 <mattdarke@fullsail.edu>
2013-05-28 13:39:24 -0700

    test

--- SDI_project_4.js
+++ SDI_project_4.js
@@ -12,17 +12,9 @@ alert("JavaScript works!");
     
     
 //Does a string follow a 123-456-7890 pattern like a phone number?    
-  var myNumber = function("123 456 7890") {
-    var phonenumber = "123-456-7890";
-    var pattern = "012-456-8900";
-    if (pattern == "123-456-7890"){
-        console.log(true)
-    } else if { (pattern != "123-456-7890")
-        return console.log(false)
-    }    
-    
-    };  
-    
-    myNumber("123 456 7890")
-    
+var myString = ("123-456-7890") {
+var myString.length;}
+ 
+//console.log(myString.length);
+        
     
