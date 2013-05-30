52f0ad13a76e7ca0611c3644972cb8acc3f3fa97
B64283 <mattdarke@fullsail.edu>
2013-05-30 12:46:23 -0700

    value of numbers in array

--- SDI_project_4.js
+++ SDI_project_4.js
@@ -12,26 +12,49 @@
 var myLibrary = function() {
   
  //Titlecase String
+
 var stringtitlecase = function() {
- var 
- words = phrase.split("matthewdarke"),
- word,
- stop =  phrase.indexof(" ");
+ var words = phrase.split("matthewdarke");
+ var stop =  phrase.indexof(" ");
  for (var i = 0; i < words.length; i++){
 	 word.substring(0, 7).toUpperCase();
-               
-	 }
+	 word.substring(1, word.length){               
 	 return words;
+	 }
+	 }
 	 };  
-  	
+ 
+ // CHECK STRING PATTERN
+ var checkString = function(val) {
+ 
+ var stringLen = val.length;
+ for (var i = 0; i < = stringLen; i ++){
+	 if ( val.substring(i, i+1) === "-"){
+      } else { 
+		console.log(true); 
+		 
+	     } 
+	 return i;
+	 } 
+ };
+ 
+///// Find the total value of just the numbers in an array
+ var totalVal = function(array);
+ var array = [ 1, "pickles", 7, "onions" "10" 5, 6, "SDI"] {
+ for(var i=0; i< 8; i++) {
+	console.log(array [0] ); 
+	} 
+ };
+ 
+ 
+ 	
 	
 }//end function
   return {
-      "stringtitlecase": string.titlecase	   
-	    
-	    
-	    
-    }//end return
+      "stringtitlecase": stringtitlecase,	   
+	   "checkString":  checkString,	    
+	    "totalVal": totalVal
+    };//end return
 
     // check email pattern
 
@@ -41,6 +64,14 @@ var stringtitlecase = function() {
 } //end my library	  
     
  // Main Code 
- var newLib = new myLibrary(); //nwe library
+ var newLib = new myLibrary(); //new library
+ 
+ console.log(newLib.stringtitlecase("matthewdarke"));
+ console.log(
+ 
+ 
+ 
+ 
+ 
  
  
